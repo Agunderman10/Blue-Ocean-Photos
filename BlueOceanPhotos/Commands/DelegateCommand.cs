d5828e96acc3fa0e52d5b167b4d42cc78ab51476
Andrew Gunderman <agunderman10@gmail.com>
2019-03-05 08:11:07 -0500

    Added DelegateCommand

--- BlueOceanPhotos/Commands/DelegateCommand.cs
+++ BlueOceanPhotos/Commands/DelegateCommand.cs
@@ -1,7 +1,22 @@
-﻿namespace BlueOceanPhotos.Commands
-{
-    class DelegateCommand
-    {
+﻿using System;
+using System.Windows.Input;
 
+namespace BlueOceanPhotos
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action _action;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
     }
 }
