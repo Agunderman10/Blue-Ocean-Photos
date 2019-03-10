b5c419c26086a017198e5187b1da4243d34fac03
Andrew Gunderman <agunderman10@gmail.com>
2019-03-10 17:34:00 -0400

    Removed height and width settings, set window center screen

--- BlueOceanPhotos/ViewModels/ImageWindowViewModel.cs
+++ BlueOceanPhotos/ViewModels/ImageWindowViewModel.cs
@@ -5,16 +5,10 @@
     public class ImageWindowViewModel
     {
         private BitmapImage _originalImage;
-        private double _imageHeight;
-        private double _imageWidth;
 
         public ImageWindowViewModel(BitmapImage originalImage)
         {
             this._originalImage = originalImage;
-            
-            //set image height and width equal to the original image's height and width
-            ImageHeight = OriginalImage.Height;
-            ImageWidth = OriginalImage.Width;
         }
 
         public BitmapImage OriginalImage
@@ -25,30 +19,6 @@
                 if(this._originalImage != value)
                 {
                     this._originalImage = value;
-                }
-            }
-        }
-
-        public double ImageHeight
-        {
-            get { return this._imageHeight; }
-            set
-            {
-                if(this._imageHeight != value)
-                {
-                    this._imageHeight = value;
-                }
-            }
-        }
-
-        public double ImageWidth
-        {
-            get { return this._imageWidth; }
-            set
-            {
-                if(this._imageWidth != value)
-                {
-                    this._imageWidth = value;
                 }
             }
         }
