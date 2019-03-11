793fad3b7b9db3a8f2742871bb518896c89aba8c
Andrew Gunderman <agunderman10@gmail.com>
2019-03-11 08:40:48 -0400

    Added RenameWindowViewModel

--- BlueOceanPhotos/Views/RenameWindow.xaml.cs
+++ BlueOceanPhotos/Views/RenameWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace BlueOceanPhotos.Views
+namespace BlueOceanPhotos
 {
     /// <summary>
     /// Interaction logic for RenameWindow.xaml
