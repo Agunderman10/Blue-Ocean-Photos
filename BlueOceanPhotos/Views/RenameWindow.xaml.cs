31afc5863df34e5c78358eb1bca63490bae70748
Andrew Gunderman <agunderman10@gmail.com>
2019-03-11 08:45:07 -0400

    Refactor

--- BlueOceanPhotos/Views/RenameWindow.xaml.cs
+++ BlueOceanPhotos/Views/RenameWindow.xaml.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace BlueOceanPhotos
+﻿namespace BlueOceanPhotos
 {
-    /// <summary>
-    /// Interaction logic for RenameWindow.xaml
-    /// </summary>
+    using System.Windows;
+
     public partial class RenameWindow : Window
     {
         RenameWindowViewModel renameWindowViewModel = new RenameWindowViewModel();
