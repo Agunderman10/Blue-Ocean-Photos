f0f0d18090d5ac1b7827e884eb9bbb5a840fa940
Andrew Gunderman <agunderman10@gmail.com>
2019-03-30 13:18:27 -0400

    Added FullScreen Viewing of ChosenImage

--- BlueOceanPhotos/ViewModels/FullScreenWindowViewModel.cs
+++ BlueOceanPhotos/ViewModels/FullScreenWindowViewModel.cs
@@ -9,9 +9,23 @@ namespace BlueOceanPhotos
 {
     public class FullScreenWindowViewModel
     {
+        private BitmapImage _chosenImage;
+
         public FullScreenWindowViewModel(BitmapImage chosenImage)
         {
+            this._chosenImage = chosenImage;
+        }
 
+        public BitmapImage ChosenImage
+        {
+            get { return this._chosenImage; }
+            set
+            {
+                if(this._chosenImage != value)
+                {
+                    this._chosenImage = value;
+                }
+            }
         }
     }
 }
